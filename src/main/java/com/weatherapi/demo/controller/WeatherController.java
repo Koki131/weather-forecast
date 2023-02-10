d4eaf01ad29e1a67d8b7f42d090ef1243ed28b1e
koki131 <zoran.stevanovich94@gmail.com>
2023-02-10 03:37:22 +0100

    Changed forecast fetching from city to lat/lon

--- src/main/java/com/weatherapi/demo/controller/WeatherController.java
+++ src/main/java/com/weatherapi/demo/controller/WeatherController.java
@@ -1,13 +1,12 @@
 package com.weatherapi.demo.controller;
 
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,9 +15,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.weatherapi.demo.model.City;
-import com.weatherapi.demo.model.Daily;
-import com.weatherapi.demo.model.WeatherData;
 import com.weatherapi.demo.service.OpenWeatherService;
+import com.weatherapi.demo.service.OpenWeatherServiceImpl;
+import com.weatherapi.demo.utility.Data;
+import com.weatherapi.demo.utility.MinMax;
 
 @Controller
 public class WeatherController {
@@ -26,6 +26,12 @@ public class WeatherController {
 	@Autowired
 	private OpenWeatherService weatherService;
 	
+	private Data data;
+	
+	@PostConstruct
+	public void init() {
+		this.data = new Data();
+	}
 	
 	@GetMapping("/") 
 	public String getHome() {	
@@ -39,7 +45,6 @@ public class WeatherController {
 		
 		List<City> cities = weatherService.getCities(city).getList();
 		
-		
 		model.addAttribute("cities", cities);
 		model.addAttribute("city", city);
 		
@@ -48,65 +53,26 @@ public class WeatherController {
 	}
 	
 	@GetMapping("/getWeather")
-	public String getWeather(@RequestParam("city") String city, Model model) {
+	public String getWeather(@RequestParam("lat") double lat, @RequestParam("lon") double lon, Model model) {
+		
+		Date date = new Date(weatherService.getWeatherMetric(lat, lon).getDt() * 1000);
+		
+		Map<Integer, MinMax> minMax = data.getMinMax(data.forecastMetric(weatherService));
 		
 		
-		Date date = new Date(weatherService.getWeatherMetric(city).getDt() * 1000);
-			
-			
+		model.addAttribute("weatherData",  weatherService.getWeatherMetric(lat, lon));
 		
-		model.addAttribute("weatherData",  weatherService.getWeatherMetric(city));
-		model.addAttribute("weatherImperial", weatherImperial(weatherService, city));
-		model.addAttribute("forecast", forecastMetric(weatherService));
+		model.addAttribute("weatherImperial", data.weatherImperial(weatherService, lat, lon));
+		model.addAttribute("forecast", data.forecastMetric(weatherService));
+		model.addAttribute("forecastImperial", data.forecastImperial(data.forecastMetric(weatherService)));
+		model.addAttribute("minMax", minMax);
 		model.addAttribute("date", date);
 		
+		System.out.println(data.forecastImperial(data.forecastMetric(weatherService)));
 				
 		return "weather";
 	}
 	
-	public static WeatherData weatherImperial(OpenWeatherService weatherService, String city) {
-		
-		
-		WeatherData imperialData = weatherService.getWeatherMetric(city);
-		
-		
-		imperialData.getMain().setTemp((int) (imperialData.getMain().getTemp() * 1.8) + 32);
-		imperialData.getMain().setFeels_like((int) (imperialData.getMain().getFeels_like() * 1.8) + 32);
-		imperialData.getMain().setTemp_min((int) (imperialData.getMain().getTemp_min() * 1.8) + 32);
-		imperialData.getMain().setTemp_max((int) (imperialData.getMain().getTemp_max() * 1.8) + 32);
-		imperialData.getWind().setSpeed((int) ((imperialData.getWind().getSpeed()) / 1.609344));
-		
-		return imperialData;
-		
-	}
+
 	
-	public static Map<Integer, List<Daily>> forecastMetric(OpenWeatherService weatherService) {
-		
-		
-		Map<Integer, List<Daily>> forecastMetric = new HashMap<>();
-		
-		
-		for (Daily forecast : weatherService.getForecastMetric().getList()) {
-			
-			
-			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-			LocalDateTime dt = LocalDateTime.parse(forecast.getDt_txt(), formatter);
-		
-			int dayOfMonth = dt.getDayOfMonth();
-			
-			if (forecastMetric.containsKey(dayOfMonth)) {
-				
-				forecastMetric.get(dayOfMonth).add(forecast);
-				
-			} else {
-				
-				forecastMetric.put(dayOfMonth, new ArrayList<>());
-				
-			}
-			
-		}
-		
-		return forecastMetric;
-		
-	}
 }
